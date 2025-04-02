a70601d047feb3d55155537652dd105c1ff7432b
Marcel Mayer <mm@servrox.solutions>
2025-04-02 09:49:42 +0200

    🚨 setup: vscode settings for linting and formatting + run it

---
+++ .prettierrc.js
@@ -0,0 +1,26 @@
+const config = {
+    trailingComma: "es5",
+    printWidth: 120, // default: 80
+    tabWidth: 2,
+    useTabs: false,
+    semi: true,
+    singleQuote: false,
+    quoteProps: "as-needed",
+    jsxSingleQuote: false,
+    trailingComma: "es5",
+    bracketSpacing: true,
+    bracketSameLine: true, // default: false
+    arrowParens: "always",
+    rangeStart: 0,
+    rangeEnd: Infinity,
+    requirePragma: false,
+    insertPragma: false,
+    proseWrap: "preserve",
+    htmlWhitespaceSensitivity: "css",
+    vueIndentScriptAndStyle: false,
+    endOfLine: "lf",
+    embeddedLanguageFormatting: "auto",
+    singleAttributePerLine: false,
+};
+
+module.exports = config;
--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -5,8 +5,7 @@ const config: Omit<Config, "content"> = {
   theme: {
     extend: {
       backgroundImage: {
-        "glow-conic":
-          "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
+        "glow-conic": "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
       },
     },
   },
